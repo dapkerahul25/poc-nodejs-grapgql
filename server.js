4b2be3896d5d641fdc6964ee9c89997b76e42204
rahul.dapke@tasind.com <rahul.dapke@tasind.com>
2022-07-21 11:49:10 +0530

    feat: log updated

--- server.js
+++ server.js
@@ -92,4 +92,4 @@ app.use(expressGraphQL({
     rootValue: root,
     graphiql: true
 }));
-app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000'));
+app.listen(4000, () => console.log(`Express GraphQL Server Now Running On localhost:4000`));
